ef48cf2d0ad8ed2b06ccaf74b6ae0899a3f2cc51
Richard Balog <balog.richard.mail@gmail.com>
2024-01-13 16:02:52 +0100

    add lesson 7

---
+++ lesson-7/src/app/private/private-details/private-details.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-private-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './private-details.component.html',
+  styleUrl: './private-details.component.scss'
+})
+export class PrivateDetailsComponent {
+  @Input() id?: string;
+}
